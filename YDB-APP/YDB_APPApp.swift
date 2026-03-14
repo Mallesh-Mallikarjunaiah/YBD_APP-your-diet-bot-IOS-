53b48041b0042767157752ae09f44b44044bbd38
Mallesh Mallikarjunaiah <mallikarjunaiah.m@northeastern.edu>
2026-03-14 12:11:40 -0400

    Initial Commit

---
+++ YDB-APP/YDB_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  YDB_APPApp.swift
+//  YDB-APP
+//
+//  Created by Mallesh Mallikarjunaiah on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct YDB_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
